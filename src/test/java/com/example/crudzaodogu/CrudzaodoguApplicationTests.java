71a175ad8c865d40db0ceb2221896a18d0f1521c
Gustavo Henrique de Oliveira <gujotoqui@gmail.com>
2025-05-08 19:47:13 -0300

    first commit

---
+++ src/test/java/com/example/crudzaodogu/CrudzaodoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudzaodogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudzaodoguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
